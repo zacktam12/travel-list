f595249a9a1e8db6a25252edaf151aa7209e00f0
hp <zekariastamiru12@gmail.com>
2024-10-22 16:02:05 +0300

    react travel-list project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Travel />
+    </div>
+  );
+}
+function Travel() {
+  return <p>This is react travel-list project</p>;
+}

b62c2f61dbbe63f4d078bc1ff194b7478be56ad6
hp <zekariastamiru12@gmail.com>
2024-10-23 02:24:42 +0300

    building the layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      <Travel />
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
-function Travel() {
-  return <p>This is react travel-list project</p>;
+function Logo() {
+  return <h1>✨Far Away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼You have X items on your list,and you already packed X(%X)</em>
+    </footer>
+  );
 }

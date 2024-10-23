89788e0f2f579edb817f53566b3e5dabb13577e8
hp <zekariastamiru12@gmail.com>
2024-10-23 20:31:58 +0300

    removing select element

--- src/App.js
+++ src/App.js
@@ -24,13 +24,7 @@ const Form = () => {
   return (
     <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
+
       <input type="text" placeholder="item..."></input>
       <button>Add</button>
     </div>

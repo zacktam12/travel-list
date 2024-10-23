9221b3721fdc9bfc268f6049dfe69013c42e91b0
hp <zekariastamiru12@gmail.com>
2024-10-23 20:35:57 +0300

    using option elemnt by direct insertion

--- src/App.js
+++ src/App.js
@@ -25,11 +25,9 @@ const Form = () => {
     <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
+        <option value={1}>1</option>
+        <option value={1}>2</option>
+        <option value={1}>3</option>
       </select>
       <input type="text" placeholder="item..."></input>
       <button>Add</button>
